0d4dc800f73499e2fa36b9b099c6825acd6dc793
Pauliina Mattila <35817764+Paul-Matt@users.noreply.github.com>
2018-06-17 13:57:44 +0300

    Repositioned map and marker to Lauttasaari, Helsinki.

--- app/src/main/java/mattila/pauliina/laruopas/MapsActivity.java
+++ app/src/main/java/mattila/pauliina/laruopas/MapsActivity.java
@@ -17,6 +17,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -29,7 +30,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker in Lauttasaari, Helsinki.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -39,8 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng lauttasaari = new LatLng(60.158611, 24.875);
+        mMap.addMarker(new MarkerOptions().position(lauttasaari).title("Marker in Lauttasaari"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(lauttasaari));
     }
 }
