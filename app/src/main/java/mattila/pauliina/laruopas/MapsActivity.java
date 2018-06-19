f42f553fddcd7071832643babcbf77a16f2ed917
Pauliina Mattila <35817764+Paul-Matt@users.noreply.github.com>
2018-06-19 12:40:14 +0300

    Added a toolbar.

--- app/src/main/java/mattila/pauliina/laruopas/MapsActivity.java
+++ app/src/main/java/mattila/pauliina/laruopas/MapsActivity.java
@@ -2,6 +2,8 @@ package mattila.pauliina.laruopas;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,7 +12,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -23,6 +25,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
     }
 
 
@@ -39,9 +47,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker in Lauttasaari and move the camera
         LatLng lauttasaari = new LatLng(60.158611, 24.875);
         mMap.addMarker(new MarkerOptions().position(lauttasaari).title("Marker in Lauttasaari"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(lauttasaari));
+
+        LatLng puun_rakkaus = new LatLng(60.145772, 24.873872);
+        mMap.addMarker(new MarkerOptions().position(puun_rakkaus).title("Puun rakkaus-veistos"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(puun_rakkaus));
     }
 }
